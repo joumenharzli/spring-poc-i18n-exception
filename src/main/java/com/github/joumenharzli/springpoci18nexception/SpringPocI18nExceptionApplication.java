81713256c66400b3d03b926777c2c5c3ca7ce911
HARZLI Joumen <harzli.joumen@gmail.com>
2017-12-19 14:15:53 +0100

    Initial commit

---
+++ src/main/java/com/github/joumenharzli/springpoci18nexception/SpringPocI18nExceptionApplication.java
@@ -0,0 +1,12 @@
+package com.github.joumenharzli.springpoci18nexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPocI18nExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPocI18nExceptionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/joumenharzli/springpoci18nexception/SpringPocI18nExceptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.joumenharzli.springpoci18nexception;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringPocI18nExceptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
